30677793a647dc47d3f503461a1386ba218c0e68
savanp08 <savanpoduturi@gmail.com>
2024-09-28 23:52:15 -0500

    empty project

---
+++ src/Templates/Schemas/event.js
@@ -0,0 +1,11 @@
+const event = {
+    "createdAt": "2024-09-29T04:50:15.475Z",
+    "_id": "65bcf52212b04b7bc3da7780",
+    "title": "Art and Culture Festival",
+    "date": "2024-06-03T00:00:00.000Z",
+    "location": "City Arts Center",
+    "category": "others",
+    "description": "Immerse in art and culture at our festival. Enjoy live performances, diverse art, and cultural displays from global talent. A vibrant celebration of creativity awaits.",
+    "image": "https://sidwellhorizon.org/wp-content/uploads/2023/10/Screenshot-2023-02-28-134748_FD871725-5056-BF65-D63E6A8D9B3EE979_fd880673-5056-bf65-d64260e7963fb5c1.jpg",
+    "seat": "800"
+}
---
+++ src/Templates/Schemas/user.js
@@ -0,0 +1,17 @@
+const user = {
+    "cover": "https://i.ibb.co/SBJcwSg/cover.png",
+    "_id": "65e1e2e49b14f6a473a74d65",
+    "name": "Organizer",
+    "email": "organizer@gmail.com",
+    "image": "https://placehold.co/500x500/png",
+    "role": "organizer",
+    "password": "$2b$10$xircfTiBDDZFj.3LCJ9X3OQcsDa4Bqa9lMubZkbeGIa1tdQVLm0ny",
+    "createdAt": "2024-03-01T14:00:17.548Z",
+    "address": "Nattagullli near dwarahat ",
+    "bio": "adsgf",
+    "city": "Nattagullli near dwarahat ",
+    "country": "India",
+    "firstname": "Yogesh",
+    "lastname": "Bhatt",
+    "phone": "7351665769"
+}
